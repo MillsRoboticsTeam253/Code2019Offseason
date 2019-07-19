9ed247f79d061dc0d6b210773099194d57c0de59
DESKTOP-GF8RGT1\aaron <aaronyu330@gmail.com>
2019-07-19 10:36:34 -0700

    Impl path loading from profiles folder

--- src/main/java/frc/robot/Auto/TrajectoryTracker.java
+++ src/main/java/frc/robot/Auto/TrajectoryTracker.java
@@ -1,5 +1,34 @@
 package frc.robot.Auto;
 
-public class TrajectoryTracker {
-    
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import jaci.pathfinder.Trajectory;
+
+public class TrajectoryTracker extends Command {
+
+    private Notifier notifier;
+    private double dt;
+    private Trajectory traj;
+
+    public TrajectoryTracker(double dt, Trajectory traj){
+        this.notifier = new Notifier(this::update);
+        this.dt = dt;
+        requires(Robot.drivetrain);
+    }
+
+    protected void initialize(){
+        notifier.startPeriodic(dt);
+    }
+
+    private void update(){
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+
 }
